c817b43b9916e27cc5e7f3981dd381863a3edd2e
Anthony Frank A. Ordoñez <143399325+AnthonyFrank-Ordonez@users.noreply.github.com>
2025-06-24 22:57:14 +0800

    conifgure allowed origins

--- src/utils/config.ts
+++ src/utils/config.ts
@@ -8,7 +8,10 @@ import { cookiesOpt } from '../types';
 const PORT = process.env.PORT!;
 const MONGODB_URI = process.env.MONGODB_URI!;
 const JWT_SECRET = process.env.JWT_SECRET!;
-const ALLOWED_ORIGINS: string[] = ['http://localhost:4173'];
+const ALLOWED_ORIGINS: string[] = [
+	'http://localhost:4173',
+	'https://ortask.afordonez.com',
+];
 
 const FRONTEND_URL =
 	process.env.NODE_ENV !== 'production'
